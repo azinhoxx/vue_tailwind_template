15877f61cf671c50c708e430022ba6d18dbf3e28
azinho <dima.statnyy@gmail.com>
2025-01-22 16:26:55 +0300

    fix: yarn migrate conflict with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,26 @@
 import antfu from '@antfu/eslint-config'
-import { FlatCompat } from '@eslint/eslintrc'
 
-const compat = new FlatCompat()
-
-export default antfu({
-  rules: {
-    'style/brace-style': ['error', '1tbs'],
+export default antfu(
+  {
+    rules: {
+      'style/brace-style': ['error', '1tbs'],
+      'antfu/top-level-function': ['off'],
+    },
   },
-}, {
-  files: ['**/*.vue'],
-  rules: {
-    'vue/max-attributes-per-line': [
-      'warn',
-      {
-        singleline: {
-          max: 1,
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/max-attributes-per-line': [
+        'warn',
+        {
+          singleline: {
+            max: 1,
+          },
+          multiline: {
+            max: 1,
+          },
         },
-        multiline: {
-          max: 1,
-        },
-      },
-    ],
+      ],
+    },
   },
-}, ...compat.config({
-  extends: ['plugin:tailwindcss/recommended'],
-  rules: {
-    'tailwindcss/classnames-order': 'warn',
-    'tailwindcss/no-unnecessary-arbitrary-value': 'warn',
-    'tailwindcss/enforces-shorthand': 'warn',
-    'tailwindcss/no-custom-classname': 'off',
-  },
-}))
+)
--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {
-      // eslint-disable-next-line node/prefer-global/process
-      ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
-    },
-  },
-}
