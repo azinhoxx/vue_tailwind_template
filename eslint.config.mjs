d61a5f3a8e1413621eb7e416f3f8db52a4449bc5
azinho <dima.statnyy@gmail.com>
2025-06-30 20:51:56 +0300

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,27 +3,31 @@ import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 
 export default antfu(
   {
-    rules: {
-      'style/brace-style': ['error', '1tbs'],
-      'antfu/top-level-function': ['off'],
+    vue: {
+      overrides: {
+        'vue/max-attributes-per-line': ['error', {
+          singleline: 1,
+          multiline: 1,
+        }],
+      },
     },
+    typescript: true,
   },
   {
-    files: ['**/*.vue'],
     rules: {
-      'import-x/first': ['off'],
-      'vue/max-attributes-per-line': [
-        'warn',
-        {
-          singleline: {
-            max: 1,
-          },
-          multiline: {
-            max: 1,
-          },
-        },
-      ],
-      'vue/brace-style': ['error', '1tbs'],
+      'ts/no-non-null-asserted-optional-chain': 'off',
+      'ts/ban-ts-comment': 'warn',
+      'ts/consistent-type-definitions': 'off',
+      'ts/no-unsafe-function-type': 'off',
+      'ts/no-empty-object-type': 'off',
+      'symbol-description': 'off',
+      'no-console': 'warn',
+      'import/first': 'off',
+      'style/max-statements-per-line': ['error', { max: 2 }],
+      'vue/one-component-per-file': 'off',
+      'unicorn/prefer-dom-node-text-content': 'off',
+      'unicorn/prefer-number-properties': 'off',
+      'regexp/no-super-linear-backtracking': 'off',
     },
   },
   {
@@ -37,13 +41,10 @@ export default antfu(
     },
     rules: {
       ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
-      'better-tailwindcss/multiline': [
-        'warn',
-        {
-          group: 'emptyLine',
-          lineBreakStyle: 'windows',
-        },
-      ],
+      'better-tailwindcss/multiline': ['warn', {
+        group: 'emptyLine',
+        lineBreakStyle: 'windows',
+      }],
       'better-tailwindcss/no-unregistered-classes': ['off'],
     },
   },
